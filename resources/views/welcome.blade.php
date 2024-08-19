a3c712fa35d20ac5648a948efe2d63f0551974e6
kumaasan <mariuszgl06@gmail.com>
2024-08-19 17:12:51 +0200

    logo and navbar test

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+<body class="">
+    <x-nav-bar></x-nav-bar>
+    <div class="w-full h-screen flex justify-center items-center bg-amber-600">
+        test
+    </div>
+
+</body>
+
+</html>
