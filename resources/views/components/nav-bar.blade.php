b063dbec05cf2da7c6c8c4418670d3edc33e55f9
kumaasan <mariuszgl06@gmail.com>
2024-08-21 20:39:28 +0200

    navbar

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,41 +1,32 @@
-<header class="bg-[#DCDCE1] text-gray-700">
-    <nav class="flex justify-between items-center w-[92%] mx-auto">
-        <div>
-            <a href="/">
-                <img class="w-20 cursor-pointer" src="{{asset('logo.svg')}}" alt="logo">
-            </a>
-        </div>
-        <div
-            class="nav-links duration-500 md:static absolute bg-[#DCDCE1] md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
-            <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 font-bold ">
-                <li>
-                    <a class="hover:text-gray-900" href="#">Strona głowna</a>
-                </li>
-                <li>
-                    <a class="hover:text-gray-900" href="#">Wszystkie przedmioty</a>
-                </li>
-                <li>
-                    <a class="hover:text-gray-900" href="#">Edytuj przedmioty</a>
-                </li>
-                <li>
-                    <a class="hover:text-gray-900" href="#">Konto</a>
-                </li>
+<header class="glassEffect text-white">
+    <nav class="flex items-center justify-between px-12 h-16 lg:gap-8 py-12">
+        <img src="{{asset('logo.svg')}}" alt="logo" class="w-20 invert-color">
+
+        <div class="dropdownMenu absolute top-24 left-0 max-lg:bg-gradient-to-br max-lg:from-[rgba(255,255,255,0.1)] max-lg:to-[rgba(255,255,255,0)] max-lg:backdrop-blur-[5px] max-lg:backdrop-filter max-lg:rounded-[5px] max-lg:border max-lg:border-[rgba(255,255,255,0.18)] max-lg:shadow-[0_8px_32px_0_rgba(0,0,0,0.37)] w-full flex flex-col gap-6 items-center py-2 text-lg font-bold lg:static lg:flex-row lg:justify-around">
+            <ul class="flex flex-col items-center gap-6 lg:flex-row lg:gap-8">
+                <li class="whitespace-nowrap">Strona główna</li>
+                <li class="whitespace-nowrap">Wszystkie przedmioty</li>
+                <li class="whitespace-nowrap">Edytuj przedmioty</li>
+                <li>Konto</li>
             </ul>
+
+            <div class="flex flex-col items-center gap-6 lg:flex-row lg:gap-8 ">
+                <button class="whitespace-nowrap">Zaloguj się</button>
+                <button class="whitespace-nowrap">Stwórz konto</button>
+            </div>
         </div>
-        <div class="flex items-center gap-6">
-            <button class="w-full text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">
-                Zaloguj się
-            </button>
-            <ion-icon onclick="onToggleMenu(this)" name="menu" class="text-3xl cursor-pointer md:hidden"></ion-icon>
+
+        <div class="toggleButton cursor-pointer lg:hidden ">
+            <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/>
+            </svg>
         </div>
     </nav>
 </header>
 
 <script>
-    const navLinks = document.querySelector('.nav-links');
+    document.querySelector('.toggleButton').addEventListener('click', function() {
+        document.querySelector('.dropdownMenu').classList.toggle('hidden');
+    });
 
-    function onToggleMenu(e) {
-        e.name = e.name === 'menu' ? 'close' : 'menu';
-        navLinks.classList.toggle('top-[9%]');
-    }
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 
     @vite('resources/css/app.css')
     <title>Document</title>
 </head>
 <body class="bg-[url('../../public/background.svg')]">
     <x-nav-bar></x-nav-bar>
-    <div class="lg:w-full lg:h-screen lg:flex lg:justify-center lg:items-center gap-4">
-        <div class="bg-yellow-500 w-1/2 h-1/5 p-40 rounded-xl">test</div>
-        <div class="bg-blue-900 w-1/3 h-1/5 p-40 rounded-xl ">test</div>
-    </div>
+        <div class="grid mx-10 grid-cols-3 grid-rows-4 gap-8 mt-3 ">
+            <div class="bg-red-500 col-span-3 flex justify-center items-center p-5 mt-9 rounded-2xl">Imie i nazwisko</div>
+            <div class="bg-amber-600 rounded-2xl ">2</div>
+            <div class="bg-yellow-500 rounded-2xl">3</div>
+            <div class="bg-blue-900 rounded-2xl">1</div>
+            <div class="bg-emerald-400 rounded-2xl">2</div>
+            <div class="bg-purple-700 rounded-2xl">3</div>
+        </div>
 
 </body>
 
